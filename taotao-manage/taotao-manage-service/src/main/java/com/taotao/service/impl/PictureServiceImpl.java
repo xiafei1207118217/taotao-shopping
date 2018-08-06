c87362520b171f2c1c2514f15cba6f161bb1e83b
xiafei <1207118217@qq.com>
2018-08-06 19:58:13 +0800

    上传图片到FTP服务器

---
+++ taotao-manage/taotao-manage-service/src/main/java/com/taotao/service/impl/PictureServiceImpl.java
@@ -0,0 +1,59 @@
+package com.taotao.service.impl;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import org.joda.time.DateTime;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.taotao.common.utils.FtpUtil;
+import com.taotao.common.utils.IDUtils;
+import com.taotao.service.PictureService;
+
+public class PictureServiceImpl implements PictureService {
+	
+	@Value("${FTP_ADDRESS}")
+	private String FTP_ADDRESS;
+	@Value("${FTP_PORT}")
+	private Integer FTP_PORT;
+	@Value("${FTP_USERNAME}")
+	private String FTP_USERNAME;
+	@Value("${FTP_PASSWORD}")
+	private String FTP_PASSWORD;
+	@Value("${FTP_BASEPATH}")
+	private String FTP_BASEPATH;
+	@Value("${IMAGE_BASE_URL}")
+	private String IMAGE_BASE_URL;
+
+	@Override
+	public Map uploadPicture(MultipartFile uploadFile){
+		Map resultmap = new HashMap<>();
+		try {
+			//生成一个新的文件名
+			//取原始文件名
+			String oldName = uploadFile.getOriginalFilename();
+			//生成新的文件名
+			String newName =IDUtils.genImageName();
+			newName = newName + oldName.substring(oldName.lastIndexOf("."));
+			//图片上传
+			String imagePath = new DateTime().toString("yyyy/MM/dd");
+			boolean result=FtpUtil.uploadFile(FTP_ADDRESS, FTP_PORT, FTP_USERNAME, FTP_PASSWORD, FTP_BASEPATH, new DateTime().toString("yyyy/MM/dd"), newName, uploadFile.getInputStream());
+			//返回结果
+			if (!result) {
+				resultmap.put("error", 1);
+				resultmap.put("message", "文件上传失败！");
+				return resultmap;
+			}
+			resultmap.put("error", 0);
+			resultmap.put("url",IMAGE_BASE_URL+ imagePath +"/"+newName);
+			return resultmap;
+		} catch (Exception e) {
+			resultmap.put("error", 1);
+			resultmap.put("message", "文件上传发生异常！");
+			return resultmap;
+		}
+	}
+
+}
---
+++ taotao-manage/taotao-manage-web/src/main/java/com/taotao/controller/PictureController.java
@@ -0,0 +1,33 @@
+package com.taotao.controller;
+/**
+ * 上传图片处理
+ * @author xiafei
+ * Title:PictureController.java
+ * Company:
+ * @date 2018年8月6日
+ */
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.taotao.common.utils.JsonUtils;
+import com.taotao.service.PictureService;
+
+public class PictureController {
+
+	@Autowired
+	PictureService pictureService;
+	
+	@RequestMapping("/pic/upload")
+	@ResponseBody
+	public String pictureUpload(MultipartFile uploadFile) {
+		Map result = pictureService.uploadPicture(uploadFile);
+		//为了保证功能的兼容性，需要把result转换成json格式的字符串
+		String json = JsonUtils.objectToJson(result);
+		return json;
+	}
+}
